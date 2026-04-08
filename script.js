9c80e04c739abe45fe5a207fe01f2f356179daaf
TSA-Websites-RJ <k1342321@students.katyisd.org>
2026-04-08 17:02:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -221,3 +221,14 @@ document.getElementById("requestForm").addEventListener("submit", e => {
 });
 
 
+const faders = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+faders.forEach(el => observer.observe(el));
